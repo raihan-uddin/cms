b9312793380d6de934b74803661a3419ca91a0c4
Raihan Uddin <raihan uddin>
2019-09-01 21:33:30 +0600

    category delete

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -24,13 +24,50 @@
 						<td>{{ $category->name }}</td>
 						<td>
 							<a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">Edit</a>
+							<button type="button" class="btn btn-sm btn-danger" onclick="handleDelete({{ $category->id }})">Delete</button>
 						</td>
 					</tr>
 					@endforeach
 				</tbody>
 			</table>
+			<!-- Modal -->
+
+			<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<form method="POST" id="deleteCategoryForm">
+						@csrf
+						@method('DELETE')
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<p class="text-center text-bold">Are you sure you want to delete this category?</p>							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">No, Go back</button>
+								<button type="submit" class="btn btn-danger">Yes, Delete</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
+@endsection
 
+@section('scripts')
+<script>
+	function handleDelete(id){
+		//console.log(id);
+		var url = '/categories/' + id;
+		var form = $('#deleteCategoryForm').attr('action', url);
+
+		$("#deleteModal").modal('show');
+
+	}
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,32 +76,36 @@
         @auth
         <div class="container">
             @if(session()->has('success'))
-                <div class="alert alert-success alert-dismissible fade show">
-                    {{ session()->get('success')}}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
+            <div class="alert alert-success alert-dismissible fade show">
+                {{ session()->get('success')}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </div>
-            @endif
-            <div class="row">
-                <div class="col-md-4">
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <a href="">Post</a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="{{route('categories.index')}}">Categories</a>
-                        </li>
-                    </ul>
+                @endif
+                <div class="row">
+                    <div class="col-md-4">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <a href="">Post</a>
+                            </li>
+                            <li class="list-group-item">
+                                <a href="{{route('categories.index')}}">Categories</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-8">
+                        @yield('content')
+                    </div>
                 </div>
-                <div class="col-md-8">
-                    @yield('content')
-                </div>
+                @else
+                @yield('content')
+                @endauth
             </div>
-        @else
-        @yield('content')
-        @endauth
-        </div>
-    </main>
-</div>
+        </main>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    @yield('scripts')
 </body>
 </html>
